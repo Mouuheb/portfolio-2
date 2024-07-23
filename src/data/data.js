ed901b69168569a852a6c04bb77f1de952762864
mouheb htiwch <93223049+Mouuheb@users.noreply.github.com>
2024-07-23 07:12:56 +0100

    header

---
+++ src/data/data.js
@@ -0,0 +1,48 @@
+const data = {
+    name:'Name',
+    pageLink:[
+        {
+            name:'About',
+            path:'',
+        },
+        {
+            name:'Skill',
+            path:'',
+        },
+        {
+            name:'Portfolio',
+            path:'',
+        },
+        {
+            name:'testimonial',
+            path:'',
+        }
+        
+
+    ],
+    navBtn:{
+        vzbl:true,
+        btnText:'Douwnload CV',
+        path:''
+            
+    },
+    header:{
+        img:'img/f1.png',
+        p: 'I’m Tanvir, a creative Product Designer. I’ve been helping businesses to solve their problems with my design for 2 years.',
+        
+        prBtn:{
+            txt:'Contact Me',
+            path:''
+        },
+        btn:{
+            txt:'View Portfolio',
+            path:''
+        },
+  
+
+            
+        
+
+    },
+};
+export default data;
--- src/pages/nav-cmp/Navcmp.jsx
+++ src/pages/nav-cmp/Navcmp.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import './nav-cmp.css'
+import data from '../../data/data'
+
 
 const Navcmp = () => {
   return (
     <div className='navcmp'>
         <div className='p1'>
-            <h1>Name</h1>
+            <h1>{data.name}</h1>
             <div className='p2'>
-                <a>About</a>
-                <a>Skill</a>
-                <a>Portfolio</a>
-                <a>testimonial</a>
+              {
+                data.pageLink.map((item,index)=>{
+                  return(
+                    <a href={item.path} key={index} >{item.name}</a>
+
+                  )
+                })
+              }
+
+                
             </div>
-            <div className='btn'>
-                <a>Download CV</a>
-            </div>
+            {data.navBtn.vzbl && <div className='btn'>
+                <a href={data.navBtn.path} >{data.navBtn.btnText}</a>
+            </div>}
             
 
         </div>
