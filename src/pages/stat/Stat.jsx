e8071c85c45bfa0d36d9202bb4cf7f29a3598f56
mouheb htiwch <93223049+Mouuheb@users.noreply.github.com>
2024-07-24 08:37:53 +0100

    stat

--- src/pages/stat/Stat.jsx
+++ src/pages/stat/Stat.jsx
@@ -1,28 +1,21 @@
 import React from 'react'
 import './stat.css'
+import data from '../../data/data'
+
 const Stat = () => {
   return (
     <div className='stat' >
-        <div className='cnt' >
-            <h1>
-                80+
-            </h1>
-            <p>xxxxxxxxxxxxx</p>
-        </div>
-        <hr/>
-        <div className='cnt' >
-            <h1>
-                80+
-            </h1>
-            <p>xxxxxxxxxxxxx</p>
-        </div>
-        <hr/>
-        <div className='cnt' >
-            <h1>
-                80+
-            </h1>
-            <p>xxxxxxxxxxxxx</p>
-        </div>
+            {data.stat.map((item,index)=>{
+                return(
+                    <>
+                        {index==0? null :<hr/>}
+                        <div className='cnt' key={index} >
+                            <h1>{item.num}</h1>
+                            <p>{item.p}</p>
+                        </div>
+                    </>
+                )
+            })}
     </div>
   )
 }
